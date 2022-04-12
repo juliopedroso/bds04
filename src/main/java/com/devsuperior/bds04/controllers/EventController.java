8567388a742d046623009571e72b659a90d2786e
Julio Cesar Pedroso <juliocp@weg.net>
2022-04-12 15:29:11 -0300

    Event insert finished

--- src/main/java/com/devsuperior/bds04/controllers/EventController.java
+++ src/main/java/com/devsuperior/bds04/controllers/EventController.java
@@ -1,15 +1,21 @@
 package com.devsuperior.bds04.controllers;
 
+import java.net.URI;
+
 import com.devsuperior.bds04.dto.EventDTO;
 import com.devsuperior.bds04.services.EventService;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/events")
@@ -25,6 +31,13 @@ public class EventController {
 
         return ResponseEntity.ok().body(list);
 
+    }
+
+    @PostMapping
+    public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 
--- src/main/java/com/devsuperior/bds04/services/EventService.java
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.bds04.services;
 
 import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.City;
 import com.devsuperior.bds04.entities.Event;
 import com.devsuperior.bds04.repositories.EventRepository;
 
@@ -19,6 +20,19 @@ public class EventService {
         Page<Event> list = repository.findAll(pageable);
        
         return  list.map(EventDTO::new);
+    }
+
+    public EventDTO insert(EventDTO dto) {
+
+        Event event = new Event();
+        event.setName(dto.getName());
+        event.setUrl(dto.getUrl());
+        event.setDate(dto.getDate());
+        event.setCity(new City(dto.getCityId(),null));
+
+        event = repository.save(event);
+
+        return new EventDTO(event);
     }
     
 }
