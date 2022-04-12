dec1a62a50adb0cf306b9eb90b8a9bd52f4929a0
Julio Cesar Pedroso <juliocp@weg.net>
2022-04-12 14:49:53 -0300

    City Controller created

--- src/main/java/com/devsuperior/bds04/services/CityService.java
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -1,8 +1,26 @@
 package com.devsuperior.bds04.services;
 
+import java.util.List;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.repositories.CityRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    public List<CityDTO> findAll() {
+
+        List<City> list = repository.findAll(Sort.by("name"));
+
+        return list.stream().map(CityDTO::new).toList();
+    }
     
 }
